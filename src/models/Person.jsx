b7614a56f48b99118a4f4603a49f5fa926a41247
dk <dk874190@gmail.com>
2024-04-01 16:29:03 +0300

    first commit

---
+++ src/models/Person.jsx
@@ -0,0 +1,11 @@
+export default class Person{
+    constructor(name,phone,email,address,experience,skills,photo){
+        this.name=name,
+        this.phone=phone,
+        this.email=email,
+        this.address=address,
+        this.experience=experience,
+        this.skills=skills,
+        this.photo=photo
+    }
+}
